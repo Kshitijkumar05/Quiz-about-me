82daa9f91616be460cce10466e3d78fdda7b82de
Kshitijkumar05 <Kshitijkumar05@users.noreply.github.com>
2020-12-01 20:12:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var readlineSync=require('readline-sync');
+
+var user=readlineSync.question("What's your name? : ");
+console.log("Welcome "+user +" to the game.");
+var score=0;
+function play(question,answer){
+  var userAnswer=readlineSync.question(question);
+
+  if (userAnswer === answer){
+    console.log("Right!");
+    score=score+1;
+  }else{
+    console.log("Wrong!");
+    score=score-1;
+  }
+  console.log("--------");
+}
+ 
+var questions = [{
+  question :"What's my age?",
+  answer :"21"},
+{ question :"where do i live?",
+answer: "Siwan"},
+{question:"My school name:", 
+answer:"msvm"}];
+
+
+for(var i=0; i<questions.length;i++){
+  var ques=questions[i];
+  play(ques.question, ques.answer)
+  
+}
+console.log("Congratulation you scored:" +score);
